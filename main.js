6f0c5158501764308d1cffdb25f7e9e18e7eb003
feras alnowiser <ferasdev@gmail.com>
2022-08-01 00:57:07 +0300

    exercise (1) : printing the array

--- main.js
+++ main.js
@@ -3,6 +3,22 @@ console.log('Week 3 - Day 1 HW')
 console.log('Tuwaiq Academy   JS')
 
 
+// Exercise (1) ------------------------------
+console.log('Exercise (1) ------------------------------');
+
+let numbers = [1, 2, 3, 5, 8, 13];
+let numbers2 = [2, 4, 8, 16, 32, 64];
+let numbers3 = [3, 6, 9, 12, 15, 18];
+
+function printArray(arr) {
+    arr.forEach(element => {
+        console.log(element);
+    });
+}
+
+printArray(numbers);
+printArray(numbers2);
+printArray(numbers3);
 
 
 
